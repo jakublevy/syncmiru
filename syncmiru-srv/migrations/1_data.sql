92fa84a3b856c09981dfb747fd7464a472cd4451
Jakub Levý <jakub.levy15@gmail.com>
2024-09-26 21:51:50 +0200

    author, desc

--- syncmiru-srv/migrations/1_data.sql
+++ syncmiru-srv/migrations/1_data.sql
@@ -1,7 +1,3 @@
-INSERT INTO "users" (
-    username, display_name, email, avatar, hash, reg_tkn_id, verified)
-VALUES ('suni', 'Suni Suni', 'ahoj@neco.cz', NULL, 'hash', NULL, FALSE);
-
 INSERT INTO "settings" (
     playback_speed, desync_tolerance, minor_desync_playback_slow, major_desync_min, room_order)
 VALUES (1.0, 2.0, 0.05, 5.0, '{}')
